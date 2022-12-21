55c6dbaf0a6a14d50a033cf30b6a3ef18f2deafa
valerialorenzo <valerialorenzo9@gmail.com>
2022-12-21 15:20:01 +0000

    excusas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+// creamos lista de listas
+let lista = [who, action, what, when];
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  // creamos lista vacia
+  let arr = [];
+  // creamos loop que recorre la lista de listas
+  for (let i in lista) {
+    let x = Math.floor(Math.random() * lista[i].length);
+    arr.push(lista[i][x]);
+  }
+
+  document.getElementById("excuse").innerHTML = arr.join(" ");
 };
